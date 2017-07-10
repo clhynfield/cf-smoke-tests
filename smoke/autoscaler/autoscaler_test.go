9540986ba450f6509634c4202c903626125f86a4
Clayton Hynfield <clayton@hynfield.org>
2017-07-10 15:09:06 -0400

    Check even longer and less often for autoscaling

--- smoke/autoscaler/autoscaler_test.go
+++ smoke/autoscaler/autoscaler_test.go
@@ -228,7 +228,7 @@ func ExpectAllAppInstancesToStart(appName string, instances int, maxAttempts int
 		if found {
 			break
 		}
-		time.Sleep(5000 * time.Millisecond)
+		time.Sleep(10000 * time.Millisecond)
 	}
 
 	Expect(found).To(BeTrue(), fmt.Sprintf("Wanted to see '%s' (all instances running) in %d attempts, but didn't", expectedOutput, maxAttempts))
